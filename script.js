5fe97ad3d55bd710ec59cf84b7b4790a56696b46
speakaccounting <147951487+speakaccounting@users.noreply.github.com>
2023-10-15 02:25:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,10 +6,20 @@ document.addEventListener("DOMContentLoaded", function () {
       const subList = this.parentElement.querySelector('.sub-list');
       const isExpanded = subList.classList.toggle('visible');
 
-      // Adjust container height
-      const container = subList.closest('.expandable-list');
-      const containerHeight = isExpanded ? subList.scrollHeight + 'px' : 'auto';
-      container.style.height = containerHeight;
+      if (isExpanded) {
+        // Get the total height of the sublist items
+        const subListHeight = Array.from(subList.children).reduce((totalHeight, listItem) => {
+          return totalHeight + listItem.clientHeight;
+        }, 0);
+
+        // Set the container height to the total height
+        const container = subList.closest('.expandable-list');
+        container.style.height = subListHeight + 'px';
+      } else {
+        // Reset container height to auto
+        const container = subList.closest('.expandable-list');
+        container.style.height = 'auto';
+      }
 
       // Toggle arrow icon
       this.textContent = isExpanded ? '▼' : '▶';
