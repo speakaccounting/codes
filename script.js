fcee032357f6788d3ed9dd88945eebfd90f05210
speakaccounting <147951487+speakaccounting@users.noreply.github.com>
2023-10-15 02:11:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,14 @@ document.addEventListener("DOMContentLoaded", function () {
   toggleElements.forEach(toggleElement => {
     toggleElement.addEventListener('click', function () {
       const subList = this.parentElement.querySelector('.sub-list');
-      subList.classList.toggle('visible');
-      this.textContent = subList.classList.contains('visible') ? '▼' : '▶';
+      const isExpanded = subList.classList.toggle('visible');
+
+      // Adjust container height
+      const container = subList.closest('.expandable-list');
+      container.style.height = isExpanded ? subList.scrollHeight + 'px' : null;
+
+      // Toggle arrow icon
+      this.textContent = isExpanded ? '▼' : '▶';
     });
   });
 });
