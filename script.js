2e3e921c6a4deefedba0fce6f2cf3ea642c676b6
speakaccounting <147951487+speakaccounting@users.noreply.github.com>
2023-10-15 02:28:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,31 +6,20 @@ document.addEventListener("DOMContentLoaded", function () {
       const subList = this.parentElement.querySelector('.sub-list');
       const isExpanded = subList.classList.toggle('visible');
 
-      if (isExpanded) {
-        // Get the total height of the sublist items
-        const subListHeight = Array.from(subList.children).reduce((totalHeight, listItem) => {
-          return totalHeight + listItem.clientHeight;
-        }, 0);
-
-        // Set the container height to the total height
-        const container = subList.closest('.expandable-list');
-        container.style.height = subListHeight + 'px';
-      } else {
-        // Reset container height to auto
-        const container = subList.closest('.expandable-list');
-        container.style.height = 'auto';
-      }
-
-      // Toggle arrow icon
-      this.textContent = isExpanded ? '▼' : '▶';
-
-      // Reset other expanded lists
-      document.querySelectorAll('.sub-list.visible').forEach(item => {
+      // Reset all lists
+      document.querySelectorAll('.sub-list').forEach(item => {
         if (item !== subList) {
           item.classList.remove('visible');
-          item.closest('.expandable-list').style.height = 'auto';
         }
       });
+
+      // Toggle arrow icon
+      toggleElements.forEach(icon => {
+        if (icon !== this) {
+          icon.textContent = '▶';
+        }
+      });
+      this.textContent = isExpanded ? '▼' : '▶';
     });
   });
 });
